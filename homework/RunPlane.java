5a0254245b1e9285ba7a1793096201269304282f
xIrang-fff <hr@webmail.hzau.edu.cn>
2017-09-21 19:57:53 +0800

    update@17.09.21
    书上第四章

--- homework/RunPlane.java
+++ homework/RunPlane.java
@@ -1,23 +1,10 @@
-package f09211;
-
-class FighterPlane{
-	 String name;
-	 int missileNum;
-	 void fire(){
-		 if (missileNum>0){
-			 System.out.println(°∞now fire a missile!°±);
-			 missileNum -=1;
-			 }
-		 else{
-			 System.out.println(°∞No missile left!°±);
-			 }
-		 } 
- 
-}
+//package f09211;
+package com.run;
+import com.resource.*;
 public class RunPlane{
 	public static void main(String args[]){
 		FighterPlane fp =new FighterPlane();
-		fp.name = "À’ 35";
+		fp.name = "√ã√ï 35";
 		fp.missileNum = 6;
 		fp.fire();
 	}
