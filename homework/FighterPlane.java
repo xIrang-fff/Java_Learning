5e4bef01f83a5ab3713d652e7254395c86536a33
xIrang-fff <hr@webmail.hzau.edu.cn>
2017-09-21 20:00:16 +0800

    update@17.09.21
    书上第四章

--- homework/FighterPlane.java
+++ homework/FighterPlane.java
@@ -1,1 +1,16 @@
 
+package com.resource;
+public class FighterPlane{
+	 public String name;
+	 public int missileNum;
+	 public void fire(){
+		 if (missileNum>0){
+			 System.out.println(“now fire a missile!”);
+			 missileNum -=1;
+			 }
+		 else{
+			 System.out.println(“No missile left!”);
+			 }
+		 } 
+ 
+} 
