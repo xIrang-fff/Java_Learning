8f769a91d7bc99af7e4109d0ee49dd50abcfaf1f
xIrang-fff <hr@webmail.hzau.edu.cn>
2017-09-21 19:27:22 +0800

    Add files via upload

---
+++ homework/MyFirstJava.java
@@ -0,0 +1,15 @@
+package f09211;
+
+
+import java.awt.*;
+import java.applet.*;
+public class MyFirstJava extends Applet{
+        public static void main(String args[]){
+        System.out.println("My first Java!");
+        }
+    public void init(){
+    }
+    public void paint(Graphics g){
+    g.drawString("My first Java!",25,25);
+    }
+}
